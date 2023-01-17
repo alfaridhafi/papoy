d45e51efc118e50e1bb72c36faccac233be7bd65
Muhammad Dhafi Alfaridzi <93527916+alfaridhafi@users.noreply.github.com>
2023-01-17 13:55:38 +0700

    tangan 17 januari

---
+++ tangan_oct07a.ino
@@ -0,0 +1,25 @@
+ /* Minimum_Source*/
+Dynamixel dxl(1);
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  dxl.begin(3);
+  dxl.setPacketType(DXL_PACKET_TYPE2);
+  dxl.setPacketType(DXL_PACKET_TYPE1);
+  
+}
+
+void loop() {
+//tegakt();
+//delay(5000);
+//walkt();
+//delay(210000);
+//tegakl();
+//jalanl();
+//lanjutanl();
+//while(1){
+//}
+tgn13jan();
+//coba();
+}
