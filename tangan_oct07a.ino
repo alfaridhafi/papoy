2bec1281980b5d0d808e26faa1f30ee91e25a933
Muhammad Dhafi Alfaridzi <93527916+alfaridhafi@users.noreply.github.com>
2023-01-19 12:32:51 +0700

    Add files via upload

---
+++ tangan_oct07a.ino
@@ -0,0 +1,27 @@
+ /* Minimum_Source*/
+Dynamixel dxl(1);
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  dxl.begin(3);
+  dxl.setPacketType(DXL_PACKET_TYPE2);
+  dxl.setPacketType(DXL_PACKET_TYPE1);
+  
+}
+
+void loop() {
+//tegakt();
+//delay(5000);
+//walkt();
+//delay(210000);
+//tegakl();
+//jalanl();
+//lanjutanl();
+//while(1){
+//}
+//iyan();
+//tgn13jan();
+//coba();
+salam_pembuka();
+}
